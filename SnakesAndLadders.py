05d54286bc0c409d2ef3a520a9911bcd621e099d
IdiotStinker <saltylightning564@gmail.com>
2024-10-31 17:25:55 +0000

    Starting board making

--- SnakesAndLadders.py
+++ SnakesAndLadders.py
@@ -1,1 +1,15 @@
+from random import randint
+
+def makeRandomBoard():
+    board = [
+        ["",]
+    ]
+    for row in range(10):
+        for col in range(10):
+            if board[row][col] != "":
+                return
+            rand = randint(1, 10)
+            if rand == 1:
+                board[row]
+            board[row][col] = 
 
